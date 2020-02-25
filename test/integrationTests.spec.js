6501547e1e57767598b7e30df14a4ba72634a815
Jon Lauridsen <mail@jonlauridsen.com>
2020-02-25 20:36:16 +0100

    Simplify integrationTests
    Switch to using external contains, and use temp dependency instead of home-cooked solution.

--- test/integrationTests.spec.js
+++ test/integrationTests.spec.js
@@ -1,22 +1,23 @@
 /* global after before describe it */
 
-const { basename, join } = require('path')
-const { strictEqual, ok } = require('assert')
+const { join } = require('path')
+const { promisify } = require('util')
+const { ok } = require('assert')
 
+const temp = require('temp').track()
 const { compile } = require('nexe')
 
+const { contains } = require('./utils')
 const { sh } = require('./spawner')
 
-function contains (content, needle) {
-  return content.indexOf(needle) > 0
-}
+/** @type {Function} */
+const tempMkdir = promisify(temp.mkdir)
 
 describe('compiled fpie2', () => {
-  let tmp
   let fpie2
 
   async function compileFpie2 () {
-    tmp = (await sh(`mktemp -d /tmp/fpie2.${basename(__filename)}.XXXXXX`)).trim()
+    const tmp = await tempMkdir('fpie2')
     fpie2 = join(tmp, 'fpie2')
     await compile({
       input: './src/index.js',
@@ -29,10 +30,6 @@ describe('compiled fpie2', () => {
     await compileFpie2()
   })
 
-  after(async function () {
-    await sh(`rm -rf ${tmp}`)
-  })
-
   it('can show help', async () => {
     const output = await sh(`${fpie2} --help`)
     ok(contains(output, 'FPIE2 tarballs a directory'), `--help should output help, got:\n${output}`)
@@ -40,12 +37,12 @@ describe('compiled fpie2', () => {
 
   it('can print files', async () => {
     const output = await sh(`${fpie2} . include.txt --print-files`, { cwd: 'test/fixtures/simple' })
-    strictEqual(output, './foo.txt\n')
+    ok(contains(output, './foo.txt'))
   })
 
   it('can output to tar', async () => {
     const output = await sh(`${fpie2} . include.txt | tar -tz`, { cwd: 'test/fixtures/simple' })
     // ⬑ "tar -t" lists content of archive to stdout
-    strictEqual(output, 'foo.txt\n')
+    ok(contains(output, 'foo.txt'))
   })
 })
