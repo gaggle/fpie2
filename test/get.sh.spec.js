6cf226551453d5e015fe1b2582ef06d7d3a4cde9
Jon Lauridsen <mail@jonlauridsen.com>
2020-02-25 20:35:35 +0100

    Simplify get.sh.spec tests
    Switch to using #contains and don't test the actual binary, it doesn't make sense to have a test like that because it relies on artifacts published to GitHub

--- test/get.sh.spec.js
+++ test/get.sh.spec.js
@@ -1,33 +1,16 @@
-const { basename } = require('path')
-const { deepStrictEqual: equal, ok } = require('assert')
+const { ok } = require('assert')
 
-const { sh, spawnPromise } = require('./spawner')
+const { contains } = require('./utils')
+const { sh } = require('./spawner')
 
 describe('get.sh', () => {
-  let tmp
-
-  before(async function () {
-    tmp = (await sh(`mktemp -d /tmp/fpie2.${basename(__filename)}.XXXXXX`)).trim()
-  })
-
-  after(async function () {
-    await sh(`rm -rf ${tmp}`)
-  })
-
   it('should calculate downloading Ubuntu release', async function () {
-    const result = await sh(`cat get.sh | UNAME_OUT=Linux DRYRUN=true VERSION=v1.0.0 TO=${tmp} sh`)
-    equal(result, `Downloading v1.0.0 of fpie2-ubuntu for Linux from https://github.com/gaggle/fpie2/releases/download/v1.0.0/fpie2-ubuntu to ${tmp}\n`)
+    const result = await sh(`cat get.sh | UNAME_OUT=Linux DRYRUN=true VERSION=v1.0.0 TO=. sh`)
+    ok(contains(result, `Downloading v1.0.0 of fpie2-ubuntu for Linux from https://github.com/gaggle/fpie2/releases/download/v1.0.0/fpie2-ubuntu to .`))
   })
 
   it('should calculate downloading Mac release', async function () {
-    const result = await sh(`cat get.sh | UNAME_OUT=Darwin DRYRUN=true VERSION=v1.0.0 TO=${tmp} sh`)
-    equal(result, `Downloading v1.0.0 of fpie2-macos for Mac from https://github.com/gaggle/fpie2/releases/download/v1.0.0/fpie2-macos to ${tmp}\n`)
-  })
-
-  it('should be able to run the downloaded binary', async function () {
-    this.timeout(60000)
-    await sh(`cat get.sh | VERSION=v1.0.0 TO=${tmp} sh`)
-    const result = await spawnPromise(`${tmp}/fpie2`, ['--help'])
-    ok(result.includes('Usage:'), `Did not find help text, got:\n${result}`)
+    const result = await sh(`cat get.sh | UNAME_OUT=Darwin DRYRUN=true VERSION=v1.0.0 TO=. sh`)
+    ok(contains(result, `Downloading v1.0.0 of fpie2-macos for Mac from https://github.com/gaggle/fpie2/releases/download/v1.0.0/fpie2-macos to .`))
   })
 })
